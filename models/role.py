ebf33758e8fd60773816a9e0217b0fa1c066afe1
Edoardo Federici <55538139+Ohswedd@users.noreply.github.com>
2024-11-28 21:06:03 +0100

    Delete models directory

--- models/role.py
+++
@@ -1,8 +0,0 @@
-from src.model import BaseModel
-from src.fields import IntegerField, StringField
-from src.relations import ManyToMany
-
-class Role(BaseModel):
-    id = IntegerField(primary_key=True)
-    name = StringField()
-    users = ManyToMany("User", pivot_table="user_roles")
